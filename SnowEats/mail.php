3d07c51a6543ce4c2ca559b12ee868e17df710a4
LeandroCeleodonio <leandroceledonio04@gmail.com>
2019-09-19 00:08:59 -0400

    PAGE SnowEats

---
+++ SnowEats/mail.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "Mail.php";
+
+$from = '<fromaddress@gmail.com>';
+$to = '<toaddress@yahoo.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow are you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'leandroceledonio04@gmail.com',
+        'password' => '19960409'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+
+?>
